1b125ca6e1c9939a0f81ee086297b077dd18b366
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-10-15 17:16:25 +0300

    Updates

---
+++ precache-manifest.3a558b892e65146a3edc77a47213cbcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5cd8fa6bc9d24099ac7ad65cda67b",
+    "url": "/HangMan/index.html"
+  },
+  {
+    "revision": "61102c4227f6de55d1a7",
+    "url": "/HangMan/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1bc5fd4218233dd62c56",
+    "url": "/HangMan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61102c4227f6de55d1a7",
+    "url": "/HangMan/static/js/2.933b4a91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/HangMan/static/js/2.933b4a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc5fd4218233dd62c56",
+    "url": "/HangMan/static/js/main.a05cf707.chunk.js"
+  },
+  {
+    "revision": "ef6db6ba2dfdaf64be1b",
+    "url": "/HangMan/static/js/runtime-main.051063a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangman/precache-manifest.711d4e5a79493faf4e7e2ef6bb404486.js"
+  "/HangMan/precache-manifest.3a558b892e65146a3edc77a47213cbcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangman/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HangMan/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
