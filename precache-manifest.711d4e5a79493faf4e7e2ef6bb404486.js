b071b67318f7eb411160dac69b3f936b0c669c32
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-10-15 17:13:00 +0300

    Updates

---
+++ precache-manifest.711d4e5a79493faf4e7e2ef6bb404486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f7124d64cdc69816d7feb603ee8e3f",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "61102c4227f6de55d1a7",
+    "url": "/hangman/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a8bdfa38715b009964db",
+    "url": "/hangman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61102c4227f6de55d1a7",
+    "url": "/hangman/static/js/2.933b4a91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hangman/static/js/2.933b4a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8bdfa38715b009964db",
+    "url": "/hangman/static/js/main.e0b3fb34.chunk.js"
+  },
+  {
+    "revision": "537c55c51d3e5d154039",
+    "url": "/hangman/static/js/runtime-main.05727f69.js"
+  }
+]);
